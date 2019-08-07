74c543bb610c02091293a83d1ebbe7cd527cd7e2
sanii-muthui <muthuisanii@gmail.com>
2019-08-07 16:57:47 +0300

    application server

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='<Flask WTF Secret Key>'
+export MAIL_USERNAME='muthuisanii@gmail.com'
+export MAIL_PASSWORD='1299Panigale'
+# export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://mannuh:123@localhost/pitch'
+
+python3.6 manage.py server
